6ead998cf13d1aa89c2489c4e1d8693cad00a883
Cecilia <cessmorah42@gmail.com>
2026-02-01 10:12:28 +0300

    added form submission event listener

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.getElementById("akanform").addEventListener("submit", function (e)){
+    e.preventDefault();
+    
+    const day = parseInt(document.getElementById("day").value);
+    const month 
+}
