bb8664f32a5401c0a33e6dc474ad6ad47f608075
Cecilia <cessmorah42@gmail.com>
2026-02-01 10:21:41 +0300

    added an if function to validate user input for date and gender

--- js/script.js
+++ js/script.js
@@ -2,5 +2,17 @@ document.getElementById("akanform").addEventListener("submit", function (e)){
     e.preventDefault();
     
     const day = parseInt(document.getElementById("day").value);
-    const month 
+    const month = parseInt(document.getElementById("month").value);
+    const year = parseInt(document.getElementById("year").value);
+
+    if (!day || !month || !year || !gender ||){
+        alert("Please fill in all fields and select your gender.");
+        return;
+    }
+    
+    if (day < 1 || day > 31 || month < 1 || month > 12){
+        alert("Invalid date or month");
+        return;
+    }
+    
 }
